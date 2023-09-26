addb0be8df65ec319d0581ea59774f4b22be0a21
shine85 <73932625+shine85@users.noreply.github.com>
2023-09-26 09:29:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,9 +11,10 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git OAF https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter' >>feeds.conf.default
 #git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
